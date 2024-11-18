5f9e1f6bfdaff425fbeda9fd63db60e7432897e0
giant-paw <giantpawbusiness@gmail.com>
2024-11-18 17:34:14 +0700

    urutkan sesuai input, semisal : nama > nim > gender > alamat > email > notelpon

--- app/src/main/java/com/example/prak7_13nov/model/Mahasiswa.kt
+++ app/src/main/java/com/example/prak7_13nov/model/Mahasiswa.kt
@@ -2,9 +2,9 @@ package com.example.prak7_13nov.model
 
 data class Mahasiswa(
     val nama : String = "",
+    val nim : String = "",
     val gender : String = "",
     val alamat : String = "",
-    val nim : String = "",
     val email : String = "",
     val notelpon : String = ""
 )
